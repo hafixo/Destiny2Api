9cc0c6413ab8dac7c6cb336a7ded57f77e97a1f9
Bailey Miller <bmiller@susquehannasoftware.com>
2017-09-06 10:20:44 -0400

    Adding some basic code for making requests

---
+++ DestinyApi/DestinyApi/ManifestEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DestinyApi
+{
+    public class ManifestEndpoint
+    {
+
+        public const string _ManifestEndpoint = "/Destiny2/Manifest/";
+
+
+        public async void GetManifest()
+        {
+            var rawData = await RootRequest.Web.GetStringAsync(_ManifestEndpoint);
+        }
+    }
+}
---
+++ DestinyApi/DestinyApi/RootRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace DestinyApi
+{
+    public static class RootRequest
+    {
+        public static String ApiKey = "831dfc2968db40a2bfd1ec9e297a7991";
+        public static String BaseUrl = "https://bungie.net/Platform";
+        public static Uri BaseUri
+        {
+            get
+            {
+                return new Uri(BaseUrl);
+            }
+        }
+        internal static HttpClient _Web { get; } = new HttpClient() { BaseAddress = BaseUri};
+        public static HttpClient Web
+        {
+            get
+            {
+                if (!_Web.DefaultRequestHeaders.Contains("X-API-Key"))
+                {
+                    _Web.DefaultRequestHeaders.Add("X-API-Key", ApiKey);
+                }
+                return _Web;
+            }
+        }
+    }
+}
