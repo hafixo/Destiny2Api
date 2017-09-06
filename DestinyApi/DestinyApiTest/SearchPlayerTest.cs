bca89005dfad632bb6c99e9194caed915765dc3b
Bailey Miller <bmiller@susquehannasoftware.com>
2017-09-06 10:58:22 -0400

    Testing of the search player works and works correctly.

---
+++ DestinyApi/DestinyApiTest/SearchPlayerTest.cs
@@ -0,0 +1,38 @@
+﻿using DestinyApi.Models.Enum;
+using DestinyApi.SearchDestinyPlayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinyApiTest
+{
+    [TestClass]
+    public class SearchPlayerTest
+    {
+        SearchDestinyPlayer _playerSearch;
+        public SearchPlayerTest()
+        {
+            _playerSearch = new SearchDestinyPlayer();
+        }
+
+
+        [TestMethod]
+        public void SearchPlayer()
+        {
+            var displayName = "GunnaKillYa";
+            var membershipType = BungieMembershipType.TigerXbox;
+            var rawData = _playerSearch.SearchDestinyPlayerAsync(displayName, membershipType).Result;
+
+
+            Assert.IsTrue(rawData.HasPlayers);
+
+            if (rawData.HasPlayers)
+            {
+                Assert.AreEqual("4611686018429590517", rawData.Response.First().membershipId);
+            }
+        }
+    }
+}
